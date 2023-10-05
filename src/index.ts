775da9d0673c5a701b48870cd48400208430426e
Haz Æ 41 <4405263+hazae41@users.noreply.github.com>
2023-10-05 21:37:39 +0200

    fix

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,5 @@
 
 import { rmSync } from "fs";
 
-for (let i = 1; i < process.argv.length; i++)
+for (let i = 2; i < process.argv.length; i++)
   rmSync(process.argv[i], { recursive: true, force: true })
